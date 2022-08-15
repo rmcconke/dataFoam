8becb5465b5f7b423be29fde6b747b68ac72d12e
Ryley McConkey <rmcconke@gra798.graham.sharcnet>
2022-08-15 07:34:28 -0400

    updates from graham

--- preprocessing/.ipynb_checkpoints/calculate_extra_fields-checkpoint.py
+++ preprocessing/.ipynb_checkpoints/calculate_extra_fields-checkpoint.py
@@ -38,6 +38,8 @@ def calc_nnls_nut_aperp(ref_field_dir,ref_case_prefix_and_name):
     a = np.load(os.path.join(ref_field_dir,ref_case_prefix_and_name+'_a.npy'))
     
     nut_nnls = np.empty(k.shape)
+    r2_nnls = np.empty(k.shape)
+
     print('    Calculating nnls nut, aperp for '+ref_case_prefix_and_name+ ' in '+ref_field_dir)
     for i in range(len(S)):
         X = -2*np.delete(S[i,:].reshape((1,9)),[3,6,7],axis=1).flatten().reshape(-1,1)
@@ -46,7 +48,7 @@ def calc_nnls_nut_aperp(ref_field_dir,ref_case_prefix_and_name):
         reg_nnls.fit(X, y)
         nut_nnls[i] = reg_nnls.coef_[0,0]
         y_pred_nnls = reg_nnls.predict(X)
-        r2_nnls = r2_score(y, y_pred_nnls)
+        r2_nnls[i] = r2_score(y, y_pred_nnls)
         if i % 10000 == 0:
             print('        Point: '+str(i)+'/'+str(len(S)))
 
@@ -62,3 +64,37 @@ def calc_nnls_nut_aperp(ref_field_dir,ref_case_prefix_and_name):
     np.save(os.path.join(ref_field_dir,ref_case_prefix_and_name+'_aperp23_nnls.npy'),aperp_nnls[:,1,2])
     np.save(os.path.join(ref_field_dir,ref_case_prefix_and_name+'_aperp33_nnls.npy'),aperp_nnls[:,2,2])
     np.save(os.path.join(ref_field_dir,ref_case_prefix_and_name+'_r2_nnls.npy'),r2_nnls)
+
+def calc_ls_nut_aperp(ref_field_dir,ref_case_prefix_and_name):
+    #tau = np.load(os.path.join(ref_field_dir,ref_case_prefix_and_name+'_tau.npy'))
+    S = np.load(os.path.join(ref_field_dir,ref_case_prefix_and_name+'_S.npy'))
+    k = np.load(os.path.join(ref_field_dir,ref_case_prefix_and_name+'_k.npy'))
+    a = np.load(os.path.join(ref_field_dir,ref_case_prefix_and_name+'_a.npy'))
+    
+    nut_nnls = np.empty(k.shape)
+    r2_nnls = np.empty(k.shape)
+
+    print('    Calculating nnls nut, aperp for '+ref_case_prefix_and_name+ ' in '+ref_field_dir)
+    for i in range(len(S)):
+        X = -2*np.delete(S[i,:].reshape((1,9)),[3,6,7],axis=1).flatten().reshape(-1,1)
+        y = np.delete(a[i,:].reshape((1,9)),[3,6,7],axis=1).flatten().reshape(-1,1)     
+        reg_nnls = LinearRegression(positive=False,fit_intercept = False)
+        reg_nnls.fit(X, y)
+        nut_nnls[i] = reg_nnls.coef_[0,0]
+        y_pred_nnls = reg_nnls.predict(X)
+        r2_nnls[i] = r2_score(y, y_pred_nnls)
+        if i % 10000 == 0:
+            print('        Point: '+str(i)+'/'+str(len(S)))
+
+    
+    aperp_nnls = a + 2*np.repeat(nut_nnls,9).reshape(len(nut_nnls),3,3)*S 
+    
+    np.save(os.path.join(ref_field_dir,ref_case_prefix_and_name+'_nut_ls.npy'),nut_nnls)
+    np.save(os.path.join(ref_field_dir,ref_case_prefix_and_name+'_aperp_ls.npy'),aperp_nnls)
+    np.save(os.path.join(ref_field_dir,ref_case_prefix_and_name+'_aperp11_ls.npy'),aperp_nnls[:,0,0])
+    np.save(os.path.join(ref_field_dir,ref_case_prefix_and_name+'_aperp12_ls.npy'),aperp_nnls[:,0,1])
+    np.save(os.path.join(ref_field_dir,ref_case_prefix_and_name+'_aperp13_ls.npy'),aperp_nnls[:,0,2])
+    np.save(os.path.join(ref_field_dir,ref_case_prefix_and_name+'_aperp22_ls.npy'),aperp_nnls[:,1,1])
+    np.save(os.path.join(ref_field_dir,ref_case_prefix_and_name+'_aperp23_ls.npy'),aperp_nnls[:,1,2])
+    np.save(os.path.join(ref_field_dir,ref_case_prefix_and_name+'_aperp33_ls.npy'),aperp_nnls[:,2,2])
+    np.save(os.path.join(ref_field_dir,ref_case_prefix_and_name+'_r2_ls.npy'),r2_nnls)
